9000f79a16def725c9bf1b2f8d12687116792cc6
alexey.khomiak <alexey.khomiak@gmail.com>
2022-05-01 15:35:16 +0300

    lang fix3

--- src/components/Body/StringLength/StringLength.jsx
+++ src/components/Body/StringLength/StringLength.jsx
@@ -2,8 +2,7 @@
 import s from './StringLength.module.css';
 import { useTranslation} from 'react-i18next';
 
-// создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = express.urlencoded({ extended: false });
+
 
 
 const StringLength = () => {
--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,22 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import translationUA from "../src/components/Language/locales/ua/translation.json";
+import translationEN from "../src/components/Language/locales/en/translation.json";
+import translationRU from "../src/components/Language/locales/ru/translation.json";
 
+
+const resources = {
+    en: {
+        translation: translationEN
+    },
+    ua: {
+        translation: translationUA
+    },
+    ru: {
+        translation: translationRU
+    }
+};
 i18n
   // load translation using http -> see /public/locales
   // learn more: https://github.com/i18next/i18next-http-backend
@@ -14,7 +29,8 @@ i18n
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
+    .init({
+    resources,
     fallbackLng: 'en',
     debug: true,
 
